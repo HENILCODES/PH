5df82f68cc3f0a8a1e9661e5593353da1f904deb
Henil <prajapatihenil1112@gmail.com>
2022-05-21 10:25:28 +0530

    henil

--- PROJECT/Calculater_t/calcu.js
+++ PROJECT/Calculater_t/calcu.js
@@ -23,24 +23,40 @@ function get_op(op) {
         document.getElementById("Answer").value = "";
         CH = op;
     }
+    else if (op == 'cc') {
+        F1 = '';
+        document.getElementById("Answer").value = '';
+    }
+    else if (op == '√') {
+        F1 = document.getElementById("Answer").value;
+        document.getElementById("Answer").value = Math.sqrt(F1);
+    }
+    else if (op == 'x²') {
+        F1 = document.getElementById("Answer").value;
+        document.getElementById("Answer").value = Math.pow(F1, 2);
+    }
     else {
         F2 = document.getElementById("Answer").value;
         switch (CH) {
             case '+':
-                document.getElementById("Answer").value = parseFloat(F1)+ parseFloat(F2);
+                AN(parseFloat(F1) + parseFloat(F2));
                 break;
             case '-':
-                document.getElementById("Answer").value = F1 - F2;
+                AN(F1 - F2);
                 break;
             case '*':
-                document.getElementById("Answer").value = F1 * F2;
+                AN(F1 * F2);
                 break;
             case '/':
-                document.getElementById("Answer").value = F1 / F2;
+                AN(F1 / F2);
                 break;
             default:
-                document.getElementById("Answer").value = "NaN";
+                document.getElementById("Answer").value = "0";
                 break;
         }
     }
+}
+
+function AN(AN) {
+    document.getElementById("Answer").value = AN;
 }
