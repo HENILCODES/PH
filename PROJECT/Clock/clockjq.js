f8e10dffbf8b22e5be742a131223962efea3ec55
HENILCODES <prajapatihenil1112@gmail.com>
2022-09-18 09:52:32 +0530

    edit clock project and add day night button

--- PROJECT/Clock/clockjq.js
+++ PROJECT/Clock/clockjq.js
@@ -1,8 +1,10 @@
 $(document).ready(function () {
-    $('#bt').click(function () {
+    $('#Mode').click(function () {
         $('body').toggleClass('body');
         $('#color').toggleClass("day");
         $('#color').toggleClass("main");
+        $("#Mode").toggleClass("active");
+        $('#moon').toggleClass("bi-moon-fill").toggleClass("bi-sun-fill");
         $('i').toggleClass("whatsapp");
     });
 });
